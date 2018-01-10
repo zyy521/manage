1102c7a8a80abfa8c4919f8120fcb436a3e7e7b4
Vip-Augus <850366301@qq.com>
2018-01-10 19:47:33 +0800

    提交

--- src/Course.js
+++ src/Course.js
@@ -1,11 +1,43 @@
-
 import React from 'react';
+import {Button, Card, Col, Form, Input, Layout, Modal, Row} from 'antd';
 
 class Course extends React.Component {
 
+    createCourse = (e) => {
+        this.setState({
+            dataVisible: true
+        })
+    }
+
+    applySubject = (e) => {
+        this.setState({
+            dataVisible: true
+        })
+    }
+
+    //切换我的实验课和所有课程
+    onCourseTypeClick(type) {
+        let obj = this;
+        this.setState({
+            courseType: type,
+            tableLoading: true
+        }, function () {
+
+        });
+    }
+
     render() {
         return (
-           <span style={{color: "red"}}>课程</span>
+            <div className="gutter-example">
+                <div className="subject-tab subject-tab-border">
+                    <a href="#" onClick={this.onCourseTypeClick.bind(this, 1)}> 我的实验课</a>
+                    <a href="#" onClick={this.onCourseTypeClick.bind(this, 2)}> 所有课程</a>
+                    <Button type="default" style={{position: "relative", left: 800}}
+                            onClick={this.createCourse}>创建实验课</Button>
+                    <Button type="primary" style={{position: "relative", left: 888}}
+                            onClick={this.applySubject}>申请课程</Button>
+                </div>
+            </div>
         )
     }
 }
